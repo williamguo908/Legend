041f12d8acca8d50bcd4ee15024ee788aa34f8c4
Deserlo <evamarienajera@csus.edu>
2019-09-30 00:25:34 -0700

    connection to db est. and init query wrks

--- api/students/all.js
+++ api/students/all.js
@@ -1,8 +1,10 @@
 
-
-const data = require('../../data.json');
+//*works*
 
 module.exports = (req, res) => {
-  const students = data.students;
-  res.status(200).json({ students });
+  const dbs = req.app.locals.db;
+  const db = dbs.db("Attendance");
+  const collection = db.collection("Attendance");
+  //const collection = req.app.locals.collection;
+  collection.find({}).toArray().then(response => res.status(200).json(response)).catch(error=> console.error(error));
 };
--- app.js
+++ app.js
@@ -3,9 +3,14 @@
 const path = require('path');
 // Dependencies
 const express = require('express');
+require('dotenv').config()
 const app = express();
 const api = require('./api')
+const port = process.env.PORT
+const mongo_uri = process.env.MONGODB_URI
+const MongoClient = require('mongodb').MongoClient;
 //const public = require('./api/public')
+//const initializeDatabase = require('./dbs')
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +20,20 @@ app.use('/', api)
 
 app.use(express.static('./api/public'));
 
+MongoClient.connect(mongo_uri, { useNewUrlParser: true })
+.then(client => {
+  const db = client
+  app.locals.db = db;
+  app.listen(port, () => console.info(`REST API running on port ${port}`));
+}).catch(error => console.error(error));
+
+
+
+
+
+
+/*
 app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is listening on port 4000');
 });
+*/
