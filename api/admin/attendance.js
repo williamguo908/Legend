01a12d250e8df608471ddb347fa6b29087fab090
Deserlo <evamarienajera@csus.edu>
2019-11-06 23:27:22 -0800

    added studentids in attendance form to check against db easily

--- api/admin/attendance.js
+++ api/admin/attendance.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 var Student = require('../../dbs/UserModel.js');
 
 module.exports = (req, res, next) => {
-  console.log("Recording attendance for class" + req.params.className)
   var students = req.body.studentId;
   var className = req.body.className;
+  console.log("Recording attendance for class" + className)
   console.log(students);
   console.log(className);
   Student.updateMany(
@@ -19,5 +19,5 @@ module.exports = (req, res, next) => {
         console.log("success with response:", res)
     }
   })
-  res.send("Attendance recorded.")
+  res.redirect('/admin/students-list') 
 }
