379963ebc16e22d66f466d8e55b6f2484af79ace
Deserlo <evamarienajera@csus.edu>
2019-11-09 14:33:44 -0800

    removed password field from showing in user profile

--- api/user/userProfile.js
+++ api/user/userProfile.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
   console.log("Retrieving your user profile...");
   const stuId = req.params.id;
   // Need to hide private notes from user view but keep in admin view..
-  Student.findOne( { '_students._id': stuId }, '-_privateNotes').exec(function (err, person) {
+  Student.findOne( { '_students._id': stuId }, '-_password').exec(function (err, person) {
     if (err) return handleError(err);
     console.log(person);
     res.send("Student Profile. " + person);
